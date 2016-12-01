1f3b478f941e7f3cbf6fa36b3d2d822fe94bf297
root <root@localhost.localdomain>
2016-12-01 02:49:24 +0100

    erst hilfe von p.. kein erfolg

--- main.c
+++ main.c
@@ -7,39 +7,39 @@ char erg[5];
 int lsg[5];
 
 printf("Lotto Tippschein eingeben: ");
-	for(int i=1; i<=10;i++){
-		for(int j=1; j<=6; j++){
+	for(int i=0; i<10;i++){
+		for(int j=0; j<6; j++){
 			scanf("%d", &tipp[i][j]);
 			printf("Zahl %d, ", j);
 			printf("Ihre Zahl %d\n", tipp[i][j]);
 		}
-		erg[i]='0';			
+		erg[i]='y';			
 
 	}
 	printf("Gewinn Zahlen eingeben: \n");
-	for(int j=1; j<=6; j++){
+	for(int j=0; j<6; j++){
                         scanf("%d", &lsg[j]);
                         printf("Zahl %d, ", j);
                         printf("Ihre Zahl %d\n", lsg[j]);
                 }
 	printf("Ihre Gewinn Zahlen: \n");
-	for(int j=1; j<=6; j++){
+	for(int j=0; j<6; j++){
                         printf("Zahl %d, ", lsg[j]);
                 }
 	printf("\n");
-	for(int i=1; i<=10;i++){
-                for(int j=1; j<=6; j++){
+	for(int i=0; i<10;i++){
+                for(int j=0; j<6; j++){
 			if(lsg[j]==tipp[i][j]){
 				erg[j] = 'x';
 			} 
 		       	printf("%d, ", tipp[i][j]);
                 }
 		printf("\n");
-		for(int l=1; l<=6; l++){
+		for(int l=0; l<6; l++){
                      printf("%c, ", erg[l]);
                 }
-		for(int l=1; l<=6; l++){
-                     erg[l]='0';
+		for(int l=0; l<6; l++){
+                     erg[l]='y';
                 }
 		printf("\n");
         }
